aea440aabbe4779c9cdbc8542538c2a3d104b83e
LittleSong2024 <idesong6@qq.com>
2025-10-04 19:44:51 +0800

    feat(core): 添加从字符串获取Hash函数的功能

--- modules/core/src/_hydrogenlib_core/hash.py
+++ modules/core/src/_hydrogenlib_core/hash.py
@@ -6,3 +6,7 @@ import hashlib
 
 def calc_hash(string: bytes, hash_type: str = "sha256"):
     return getattr(hashlib, hash_type)(string)
+
+
+def get_hash_func(hash_type: str = "sha256"):
+    return getattr(hashlib, hash_type)
