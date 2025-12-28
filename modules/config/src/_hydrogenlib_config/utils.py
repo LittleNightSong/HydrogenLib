c02ea43771cb04fb3d59564d79b071bada6bc0e9
LittleSong2025 <142714722+LittleSong2025@users.noreply.github.com>
2025-12-28 18:21:09 +0800

    refactor: 更新配置模块，添加格式注册功能并改进类型注解
    - 添加 `registry_data_format` 函数以支持自定义数据格式
    - 为 `load_from_obj` 函数添加 `ExtraActions` 类型注解
    - 修正 `format_mro` 中的格式信息字典键值对对齐问题

--- modules/config/src/_hydrogenlib_config/utils.py
+++ modules/config/src/_hydrogenlib_config/utils.py
@@ -1,6 +1,5 @@
 import ast
 import json
-
 from typing import Literal, Callable, Any
 
 from .core.base import ConfigBase
@@ -16,15 +15,20 @@ format_mro = {  # type: ignore
     'python': {
         'load-io': ast.literal_eval,
         'load-file': 'open',
-        'dump-io':  repr,
+        'dump-io': repr,
         'dump-file': 'open'
     }
 }
 
+type SpecialActions = Literal['open']
+type ExtraActions = Literal['ignore', 'allow', 'error']
+
+
 def has_field(cfg: ConfigBase, name: str):
     return cfg.__config_fields__.__contains__(name)
 
-def load_from_obj(cfg, obj, extra='ignore'):
+
+def load_from_obj(cfg, obj, extra: ExtraActions = 'ignore'):
     for k, v in obj.items():
         if not has_field(cfg, k):
             match extra:
@@ -37,6 +41,7 @@ def load_from_obj(cfg, obj, extra='ignore'):
         else:
             setattr(cfg, k, v)
 
+
 def load_from_file(cfg, file, format='json'):
     format_info = format_mro[format]
 
@@ -46,6 +51,7 @@ def load_from_file(cfg, file, format='json'):
             return load_from_obj(cfg, format_info['load-io'](f))
     else:
         return load_function(file)
+
 
 def load_from_io(cfg, io, format='json'):
     format_info = format_mro[format]
@@ -68,3 +74,19 @@ def dump_to_io(cfg, io, format='json'):
     format_info = format_mro[format]
 
     return format_info['dump-io'](io, cfg.to_dict())
+
+
+def registry_data_format(
+        format: str,
+        load_io: Callable[[Any], Any],
+        dump_io: Callable[[Any], Any],
+        load_file: Callable[[Any], Any] | SpecialActions = 'open',
+        dump_file: Callable[[Any], Any] | SpecialActions = 'open'
+):
+    format_info = {
+        'load-io': load_io,
+        'load-file': load_file,
+        'dump-io': dump_io,
+        'dump-file': dump_file,
+    }
+    format_mro[format] = format_info
