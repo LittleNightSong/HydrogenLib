ad2850381e618c944f7dac674d4b3788f36b31dd
LittleNightSong <LittleNightSongYO@outlook.com>
2026-02-13 10:35:26 +0800

    refactor(config): Import global type registry and re-import simple utils

--- modules/config/src/_hydrogenlib_config/simple/__init__.py
+++ modules/config/src/_hydrogenlib_config/simple/__init__.py
@@ -3,3 +3,4 @@ from .builtin_mixins import NamedTupleMixin
 from .mixin import Mixin
 from _hydrogenlib_config_core.field import FieldInfo, Field
 from _hydrogenlib_config_core.type_registry import TypeRegistry
+from .gtr import gtr as global_type_registry
